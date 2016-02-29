1e6b3690fbdb5acaf29da715c4d30d285f63b138
Long Luc <culgnol@users.noreply.github.com>
2016-02-29 00:12:06 -0800

    Create conf.js

---
+++ conf.js
@@ -0,0 +1,13 @@
+// conf.js
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
+
+  capabilities: { 'browserName': 'chrome' },
+
+  jasmineNodeOpts: {
+    isVerbose: true,
+    showColors: true
+  }
+};
